29da4c25855c6067bee9def954046b2c92612fdb
Intax <48190928+intaxwashere@users.noreply.github.com>
2021-08-03 08:57:09 +0300

    Update TickCollectionSubsystem.h

--- TickCollectionPlugin/Source/TickCollectionPlugin/Public/TickCollectionSubsystem.h
+++ TickCollectionPlugin/Source/TickCollectionPlugin/Public/TickCollectionSubsystem.h
@@ -9,6 +9,10 @@
 DECLARE_STATS_GROUP(TEXT("TickCollectionPlugin"), STATGROUP_TickCollectionPlugin, STATCAT_Advanced);
 DECLARE_CYCLE_STAT(TEXT("Total Collector Tick Time"), STAT_TickCollectorTotal, STATGROUP_TickCollectionPlugin);
 
+// We dont want runtime allocations of ObjectCollection because they are very expensive. So instead we can
+// allocate them all in the beginning. This constexpr variable also gonna be calculated in compile-time.
+constexpr int32 ReserveCount = 200;
+
 UCLASS()
 class TICKCOLLECTIONPLUGIN_API UTickCollectionWorldSubsystem : public UWorldSubsystem, public FTickableGameObject
 {
@@ -23,6 +27,8 @@ class TICKCOLLECTIONPLUGIN_API UTickCollectionWorldSubsystem : public UWorldSubs
 	{
 		Super::Initialize(Collection);
 		bInitialized = true;
+		
+		ObjectCollection.Reserve(ReserveCount);
 	}
 
 	/* The actual collection data. */
