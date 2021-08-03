492fa65e5322e823966db652caffae2e8d46cfe5
Intax <48190928+intaxwashere@users.noreply.github.com>
2021-08-03 08:42:08 +0300

    Update TickCollectionSubsystem.cpp

--- TickCollectionPlugin/Source/TickCollectionPlugin/Private/TickCollectionSubsystem.cpp
+++ TickCollectionPlugin/Source/TickCollectionPlugin/Private/TickCollectionSubsystem.cpp
@@ -6,27 +6,31 @@
 void UTickCollectionWorldSubsystem::Tick(float DeltaTime)
 {
 	SCOPE_CYCLE_COUNTER(STAT_TickCollectorTotal);
-	if(bInitialized == false) return;
-		
+	
+	if(bInitialized == false || ObjectCollection.IsValidIndex(0) == false) 
+	{
+		return;
+	}
+	
 	const int32 Count = GetCollectionCount();
 	for(int16 i = 0; i <= Count; ++i)
 	{
-		if(ObjectCollection.IsValidIndex(i))
+		UObject* Object = ObjectCollection[i];
+		if(IsValid(Object))
 		{
-			UObject* Object = ObjectCollection[i];
-			if(IsValid(Object))
+			// So as you can see, I'm using CastChecked<>() here, so its better if you ensure you have the interface implemented.
+			// The reason we are using CastChecked<>() is, its being converted to static cast in packaged builds. Which is faster than dynamic casting.
+			ITickCollectionInterface* Interface = CastChecked<ITickCollectionInterface>(Object);
+			if(Interface->bCanCollectionTick == false)
 			{
-				ITickCollectionInterface* Interface = CastChecked<ITickCollectionInterface>(Object);
-				if(Interface->bCanCollectionTick == false)
-				{
-					continue;
-				}
-				Interface->CollectionTick(DeltaTime);
+				continue;
 			}
-			else
-			{
-				ObjectCollection.RemoveAt(i);
-			}
+			Interface->CollectionTick(DeltaTime);
+		}
+		else
+		{
+			ObjectCollection.RemoveAt(i);
 		}
 	}
 }
+
